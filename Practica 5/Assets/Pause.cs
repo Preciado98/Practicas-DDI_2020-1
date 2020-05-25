ed60c9b7f498bea2c6899e6b1a059d25cfe8f55f
preciado98 <preciado.guillermina@uabc.edu.mx>
2020-05-25 12:53:05 -0700

    Practica 5

---
+++ Practica 5/Assets/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Threading;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    bool activo;
+    Canvas canvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+            activo = !activo;
+            canvas.enabled = activo;
+            Time.timeScale = (activo) ? 0 : 1f;
+        }
+    }
+}
