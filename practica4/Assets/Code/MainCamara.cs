91782bc95eecd682a59b4cbb9a274258c2a62b69
preciado98 <preciado.guillermina@uabc.edu.mx>
2020-05-27 22:52:10 -0700

    Practicas

---
+++ practica4/Assets/Code/MainCamara.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamara : MonoBehaviour {
+
+    Transform target;
+    float tLx, tLY, bRx, bRY;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(
+            Mathf.Clamp(target.position.x,tLx,bRx),
+            Mathf.Clamp(target.position.y,bRY, tLY),
+            transform.position.z);
+	}
+
+    public void SetBound(GameObject map) {
+        Tiled2Unity.TiledMap config = map.GetComponent<Tiled2Unity.TiledMap>();
+        float cameraSize = Camera.main.orthographicSize;
+
+        tLx = map.transform.position.x + cameraSize;
+        tLY = map.transform.position.y - cameraSize;
+        bRx = map.transform.position.x + config.NumTilesWide - cameraSize;
+        bRY = map.transform.position.y - config.NumTilesHigh + cameraSize;
+    }
+
+}
---
+++ practica4/Assets/Code/Playermov.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Playermov : MonoBehaviour{
+    
+    public float speed = 4f;
+    Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov;
+
+    public GameObject initialMap;
+
+    void Awake()
+    {
+        Assert.IsNotNull(initialMap);
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+        Camera.main.GetComponent<MainCamara>().SetBound(initialMap);
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            );
+
+        if (mov != Vector2.zero)
+        {
+            anim.SetFloat("movX", mov.x);
+            anim.SetFloat("movY", mov.y);
+            anim.SetBool("walking",true);
+        }
+        else {
+            anim.SetBool("walking",false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    }
+
+}
