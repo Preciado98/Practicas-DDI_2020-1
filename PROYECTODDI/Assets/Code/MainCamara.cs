a7dad4dfcbe4b6e74f4572b56ec61f744095c2a2
preciado98 <preciado.guillermina@uabc.edu.mx>
2020-05-28 18:15:05 -0700

    Proyecto DDI V1

---
+++ PROYECTODDI/Assets/Code/MainCamara.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamara : MonoBehaviour {
+
+    public float smoothTime = 1.25f;
+
+    Transform target;
+    float tLx, tLY, bRx, bRY;
+    Vector2 velocity;
+
+	// Use this for initialization
+	void Awake () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+    void Start()
+    {
+        Screen.SetResolution(800, 800, true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (!Screen.fullScreen || Camera.main.aspect != 1) {
+            Screen.SetResolution(800, 800, true);
+        }
+        if (Input.GetKey("escape")) Application.Quit();
+
+        float posX = Mathf.Round(
+            Mathf.SmoothDamp(transform.position.x,
+            target.position.x, ref velocity.x,smoothTime
+            )* 100) / 100;
+
+        float posY = Mathf.Round(
+            Mathf.SmoothDamp(transform.position.y,
+            target.position.y, ref velocity.y, smoothTime
+            ) * 100) / 100;
+
+
+        transform.position = new Vector3(
+            Mathf.Clamp(posX,tLx,bRx),
+            Mathf.Clamp(posY,bRY, tLY),
+            transform.position.z);
+	}
+
+    public void SetBound(GameObject map) {
+        Tiled2Unity.TiledMap config = map.GetComponent<Tiled2Unity.TiledMap>();
+        float cameraSize = Camera.main.orthographicSize;
+
+        tLx = map.transform.position.x + cameraSize;
+        tLY = map.transform.position.y - cameraSize;
+        bRx = map.transform.position.x + config.NumTilesWide - cameraSize;
+        bRY = map.transform.position.y - config.NumTilesHigh + cameraSize;
+
+        FastMove();
+    }
+
+    public void FastMove() {
+        transform.position = new Vector3(
+            target.position.x,
+            target.position.y,
+            transform.position.z);
+    }
+
+}
---
+++ PROYECTODDI/Assets/Enemy.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float visionRadius;
+    public float attackRadius;
+    public float speed;
+
+    GameObject player;
+
+    Vector3 initialPosition;
+
+    Animator anim;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        initialPosition = transform.position;
+
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+        Vector3 target = initialPosition;
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            player.transform.position - transform.position,
+            visionRadius,
+            1 << LayerMask.NameToLayer("Default")
+        );
+
+        Vector3 forward = transform.TransformDirection(player.transform.position - transform.position);
+        Debug.DrawRay(transform.position, forward, Color.red);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Player")
+            {
+                target = player.transform.position;
+            }
+        }
+
+        float distance = Vector3.Distance(target, transform.position);
+        Vector3 dir = (target - transform.position).normalized;
+
+        if (target != initialPosition && distance < attackRadius)
+        {
+            anim.SetFloat("movX", dir.x);
+            anim.SetFloat("movY", dir.y);
+            anim.Play("Enemy_Walk", -1, 0);
+        }
+        else
+        {
+            rb2d.MovePosition(transform.position + dir * speed * Time.deltaTime);
+
+            anim.speed = 1;
+            anim.SetFloat("movX", dir.x);
+            anim.SetFloat("movY", dir.y);
+            anim.SetBool("walking", true);
+        }
+
+        if (target == initialPosition && distance < 0.02f)
+        {
+            transform.position = initialPosition;
+            anim.SetBool("walking", false);
+        }
+
+        Debug.DrawLine(transform.position, target, Color.green);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRadius);
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+    }
+}
